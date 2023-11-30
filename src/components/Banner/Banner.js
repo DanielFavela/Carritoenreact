31240ebab698beccac0e757a116befb0847c566e
DanielFavela <19040356@itdurango.edu.mx>
2023-11-30 00:48:59 -0600

    first commit
    first commit

---
+++ src/components/Banner/Banner.js
@@ -0,0 +1,12 @@
+import "./Banner.css";
+const Banner = () => {
+  return (
+    <div className="banner">
+       <div className="banner-container">
+        <h1>Tienda Fitness</h1>
+        <p>Para persononas con gustos deliciosos</p>
+        </div>
+        </div>
+  )
+}
+export default Banner;
